dee9dc54fa530b26089f4a58c5eff995cc8efb3b
shreyaPatike <lit2020058@iiitl.ac.in>
2024-01-21 23:27:29 +0530

    views set to app

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 9000;
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + "/frontend"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
